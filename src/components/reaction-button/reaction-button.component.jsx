5fe9d10314eb20170777c675d18651d014d246c3
JamesGabby <jlghfe36@gmail.com>
2022-02-25 14:20:10 +0000

    updated attempts

--- src/components/reaction-button/reaction-button.component.jsx
+++ src/components/reaction-button/reaction-button.component.jsx
@@ -6,6 +6,7 @@ import GameOver from '../../pages/game-over/game-over';
 var clickedTime, changeTime, reactionTime, total, average;
 var reactionTimeArray = [];
 var clicks = [];
+var count = [];
 
 var hasStarted = false;
 var gameOver = false;
@@ -32,13 +33,13 @@ function ReactionButton() {
   return (
     <div className="App">
       <div>
-        <button className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          style={{padding: '1rem', marginTop: '7rem', cursor: 'pointer'}} 
+        <button className="inline-flex justify-center py-2 px-4 p-5 mt-20 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           onClick={() => {
             hasStarted = true;
             gameOver = false;
             changeColour();
             reactionTimeArray = reactionTimeArray.filter((prev) => prev === '');
+            count = count.filter((prev) => prev === 'Z');
             average = 0;
         }}>
           {gameOver ? 'Play Again' : 'Click to begin'}
@@ -50,16 +51,16 @@ function ReactionButton() {
           ? 
             <GameOver /> 
           : 
-            <button className={`inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white ${colour} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
-              style={{padding: '3rem', marginTop: '3rem', cursor: 'pointer'}} 
+            <button className={`inline-flex justify-center p-10 mt-20 border border-transparent shadow-sm text-sm font-medium rounded-md text-white ${colour} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
               disabled={!hasStarted}
               onClick={function() {
                 clicks.push({});
+                count.push({});
                 clickedTime = Date.now();
                 reactionTime = (clickedTime - changeTime) / 1000;    
                 reactionTimeArray.push({
                   reactionTime: reactionTime,
-                  id: Math.random()
+                  id: count.length
                 });  
                 total = reactionTimeArray.reduce((accumulator, currentElement) => accumulator + currentElement.reactionTime, 0);
                 average = (total / reactionTimeArray.length).toFixed(3);
@@ -83,7 +84,7 @@ function ReactionButton() {
       <h2 className="text-3xl font-bold my-5">{gameOver ? `Final Score: ${average}` : ''}</h2>
       <h3 className="text-2xl font-bold">{gameOver ? reactionTimeArray.map((rt) => (
         <div key={rt.id}>
-          {rt.reactionTime} seconds
+         Attempt {rt.id}: {rt.reactionTime} seconds
         </div>
         )) : ''}
       </h3>
